27ba089a79a7717b46dd8f549c1a804b073c8a0b
jesusmanuelgaytan09-collab <jesusmanuelgaytan09@gmail.com>
2025-08-28 15:31:22 -0700

    Add files via upload

---
+++ EVA1_4_DATOS/src/eva1_4_datos/EVA1_4_DATOS.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_4_datos;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_4_DATOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    System.out.print("Empleo: ");
+    System.out.println("Gerente de HYM");
+    System.out.print("salario: ");
+    System.out.println("27000");
+    System.out.print("Horas a la semana: ");
+    System.out.println("44");
+    System.out.print("Requiere ingles?: ");
+    System.out.println("true");
+    }
+    
+}
