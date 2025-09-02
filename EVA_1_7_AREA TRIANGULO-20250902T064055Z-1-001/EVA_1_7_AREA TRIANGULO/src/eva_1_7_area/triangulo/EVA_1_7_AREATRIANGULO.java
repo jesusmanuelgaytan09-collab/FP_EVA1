2083ff26e123706650d7b36ffd86bc5865719307
jesusmanuelgaytan09-collab <jesusmanuelgaytan09@gmail.com>
2025-09-02 00:43:18 -0600

    Add files via upload

---
+++ EVA_1_7_AREA TRIANGULO-20250902T064055Z-1-001/EVA_1_7_AREA TRIANGULO/src/eva_1_7_area/triangulo/EVA_1_7_AREATRIANGULO.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva_1_7_area.triangulo;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA_1_7_AREATRIANGULO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double base, altura, area;
+        base = 5;
+        altura = 10;         
+        area =(base*altura)/2;
+        System.out.println("El area de un triangulo con base de 5 y altura de 10 =");
+        System.out.println(area);
+       
+    }
+    
+}
