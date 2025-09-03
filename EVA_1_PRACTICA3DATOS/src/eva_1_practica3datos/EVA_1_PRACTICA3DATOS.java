27c3acf4d598e75498019f7fb38145e443e66e47
jesusmanuelgaytan09-collab <jesusmanuelgaytan09@gmail.com>
2025-09-03 02:34:35 -0600

    Add files via upload

---
+++ EVA_1_PRACTICA3DATOS/src/eva_1_practica3datos/EVA_1_PRACTICA3DATOS.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_practica3datos;
+
+/**
+ *
+ * @author range
+ */
+public class EVA_1_PRACTICA3DATOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    System.out.println("NOMBRE DEL ESTUDIANTE: ");
+    System.out.println("JESUS MANUEL GAYTAN RANGEL");
+    System.out.println("NUMERO DE CONTROL: ");
+    System.out.println(25550722);
+    System.out.println("EDAD: ");
+    System.out.println(19);
+    }
+    
+}
