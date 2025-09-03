f73317182383cd05c59a40aa1d4ecce29d22945e
jesusmanuelgaytan09-collab <jesusmanuelgaytan09@gmail.com>
2025-09-03 02:10:22 -0600

    Add files via upload

---
+++ EVA_1_PRACTICA1HOLAMUNDO/src/eva_1_practica1holamundo/EVA_1_PRACTICA1HOLAMUNDO.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_practica1holamundo;
+
+/**
+ *
+ * @author range
+ */
+public class EVA_1_PRACTICA1HOLAMUNDO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    System.out.println("HOLA MUNDO");
+    System.out.println("FUNDAMENTOS DE PROGRAMACION");
+    }
+    
+}
