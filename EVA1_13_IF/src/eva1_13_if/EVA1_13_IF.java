10661b50fb0b3a8844f36b701cec079041373af4
jesusmanuelgaytan09-collab <jesusmanuelgaytan09@gmail.com>
2025-09-10 03:45:19 -0600

    Add files via upload

---
+++ EVA1_13_IF/src/eva1_13_if/EVA1_13_IF.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_13_if;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author range
+ */
+public class EVA1_13_IF {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("introduce la edad");
+    venta = captu.nextInt();
+    int venta;
+    if (venta <= 18){
+    System.out.println("si se ´puede venta");
+    }else{
+        System.out.println("no se puede venta");
+    }
+    
+    
+    
+    }
+    
+}
